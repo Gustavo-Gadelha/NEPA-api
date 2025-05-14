2fcfc24b1d50437e4b4d95ba5122adc88b04fc3e
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-05-14 07:22:05 -0300

    refactor: mudanças nos mixins para reduzir conflitos durante implementações

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -6,29 +6,32 @@ from app import db
 
 
 @declarative_mixin
-class TimestampMixin(object):
+class TimestampMixin:
     criado_em = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
-    atualizado_em = db.Column(db.DateTime(timezone=True), onupdate=func.now(), nullable=True)
+    atualizado_em = db.Column(db.DateTime(timezone=True), server_onupdate=func.now(), nullable=True)
 
 
 @declarative_mixin
 class LogMixin:
-    @declared_attr.directive
+    @declared_attr
     def criado_por(cls):
         return db.Column(db.UUID(as_uuid=True), db.ForeignKey('usuario.id'), nullable=True)
 
-    @declared_attr.directive
+    @declared_attr
     def atualizado_por(cls):
         return db.Column(db.UUID(as_uuid=True), db.ForeignKey('usuario.id'), nullable=True)
 
+    @classmethod
+    def __declare_last__(cls):
+        db.event.listen(cls, 'before_insert', cls._log_criado_por)
+        db.event.listen(cls, 'before_update', cls._log_atualizado_por)
 
-@db.event.listens_for(LogMixin, 'before_insert')
-def before_update_listener(mapper, connection, target):
-    if current_user:
-        target.criado_por = current_user.id
+    @staticmethod
+    def _log_criado_por(mapper, connection, target):
+        if current_user and hasattr(target, 'criado_por'):
+            target.criado_por = current_user.id
 
-
-@db.event.listens_for(LogMixin, 'before_update')
-def before_update_listener(mapper, connection, target):
-    if current_user:
-        target.atualizado_por = current_user.id
+    @staticmethod
+    def _log_atualizado_por(mapper, connection, target):
+        if current_user and hasattr(target, 'atualizado_por'):
+            target.atualizado_por = current_user.id
