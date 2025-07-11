8acaf4e34294cbaeb2f2ae0f57e109b56e42d113
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-07-11 10:00:41 -0300

    feat: suporte a blueprints aninhados com registro recursivos

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -6,17 +6,29 @@ from .cursos import curso_blp
 from .editais import edital_blp
 from .professores import professor_blp
 from .projetos import projeto_blp
+from .projetos.incricoes import inscricao_blp
 
-blueprints = [
-    auth_blp,
-    curso_blp,
-    aluno_blp,
-    professor_blp,
-    edital_blp,
-    projeto_blp,
-]
+routes = {
+    '/auth': auth_blp,
+    '/cursos': curso_blp,
+    '/editais': edital_blp,
+    '/alunos': aluno_blp,
+    '/professores': professor_blp,
+    '/projetos': {
+        '/': projeto_blp,
+        '/<uuid:projeto_id>/inscricoes': inscricao_blp
+    },
+}
 
 
-def register_blueprints(api: Api):
-    for blp in blueprints:
-        api.register_blueprint(blp)
+def register_blueprints(api: Api, blueprints=None, parent_prefix: str = ''):
+    if blueprints is None:
+        blueprints = routes
+
+    for prefix, blp in blueprints.items():
+        full_prefix = parent_prefix + prefix
+
+        if isinstance(blp, dict):
+            register_blueprints(api, blp, full_prefix)
+        else:
+            api.register_blueprint(blp, url_prefix=full_prefix)
