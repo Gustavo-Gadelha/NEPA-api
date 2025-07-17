96d65837a3d0a167990da3031c8ad7b04f21a78a
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-07-16 23:52:03 -0300

    feat: restringe acesso de professor apenas para frequências de seus projetos

--- app/resources/controles/services.py
+++ app/resources/controles/services.py
@@ -1,9 +1,14 @@
+from uuid import UUID
+
 from app.core import CRUDService
 from app.models import ControleMensal
 
 
 class ControleMensalService(CRUDService[ControleMensal]):
     model = ControleMensal
+
+    def owns_controle(self, _id: UUID, professor_id: UUID) -> bool:
+        return self.get_or_404(_id).professor_id == professor_id
 
 
 controle_mensal_service = ControleMensalService()
--- app/resources/frequencias/routes.py
+++ app/resources/frequencias/routes.py
@@ -1,12 +1,11 @@
 from flask.views import MethodView
 from flask_jwt_extended import current_user
 from flask_smorest import Blueprint
-from werkzeug.exceptions import Forbidden, NotFound
+from werkzeug.exceptions import Forbidden
 
 from app.jwt import requires_any
 from app.models.enums import Autoridade
 from app.resources.controles import controle_mensal_service
-from app.resources.projetos import projeto_service
 from .schemas import FrequenciaSemanalInSchema, FrequenciaSemanalArgsSchema, FrequenciaSemanalOutSchema
 from .services import frequencia_semanal_service
 
@@ -20,14 +19,19 @@ class FrequenciaList(MethodView):
     @frequencia_blp.arguments(FrequenciaSemanalArgsSchema, location='query', as_kwargs=True)
     @frequencia_blp.response(200, FrequenciaSemanalOutSchema(many=True))
     def get(self, controle_id, **kwargs):
-        return frequencia_semanal_service.get_all(controle_id=controle_id, **kwargs)
+        if current_user.autoridade == Autoridade.ADMIN:
+            return frequencia_semanal_service.get_all(controle_id=controle_id, **kwargs)
+        if controle_mensal_service.owns_controle(controle_id, current_user.id):
+            return frequencia_semanal_service.get_all(controle_id=controle_id, **kwargs)
+
+        raise Forbidden
 
     @requires_any(Autoridade.PROFESSOR)
     @frequencia_blp.arguments(FrequenciaSemanalInSchema)
     @frequencia_blp.response(201, FrequenciaSemanalOutSchema)
     def post(self, frequencia, controle_id):
-        if not projeto_service.is_owner(controle_id.projeto_id, current_user.id):
-            raise Forbidden
+        if not controle_mensal_service.owns_controle(controle_id, current_user.id):
+            raise Forbidden('Este professor não pode acessar esse controle mensal')
 
         frequencia.controle_mensal_id = controle_id
         return frequencia_semanal_service.save(frequencia)
@@ -39,18 +43,17 @@ class FrequenciaDetail(MethodView):
     @requires_any(Autoridade.ADMIN, Autoridade.PROFESSOR)
     @frequencia_blp.response(200, FrequenciaSemanalOutSchema)
     def get(self, controle_id, frequencia_id):
-        controle_mensal = controle_mensal_service.get_or_404(controle_id)
-        frequencia_semanal = frequencia_semanal_service.get_or_404(frequencia_id)
+        if current_user.autoridade == Autoridade.ADMIN:
+            return frequencia_semanal_service.get_or_404(frequencia_id)
+        if controle_mensal_service.owns_controle(controle_id, current_user.id):
+            return frequencia_semanal_service.get_or_404(frequencia_id)
 
-        if frequencia_semanal not in controle_mensal.frequencias_semanais:
-            raise NotFound
-
-        return frequencia_semanal
+        raise Forbidden
 
     @requires_any(Autoridade.PROFESSOR)
     @frequencia_blp.response(204)
     def delete(self, controle_id, frequencia_id):
-        if not projeto_service.is_owner(controle_id.projeto_id, current_user.id):
-            raise Forbidden
+        if not controle_mensal_service.owns_controle(controle_id, current_user.id):
+            raise Forbidden('Este professor não pode acessar essa frequência')
         
         return frequencia_semanal_service.delete_by_id(frequencia_id)
