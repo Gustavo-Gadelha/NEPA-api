0a081d7cc62106876ba526885863414723371a51
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-07-28 09:25:19 -0300

    refactor: reorganiza e adiciona novos atributos no módulo de configurações

--- app/config.py
+++ app/config.py
@@ -28,47 +28,54 @@ ALLOWED_ANEXOS_EXTENSIONS: set[str] = {
     'jpg',
 }
 MAX_FILE_SIZE: int = 8 * 1024 * 1024  # 8 MB
+
 MIN_PASSWORD_LENGTH: int = 8
 
 
 class _Config:
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    MAX_CONTENT_LENGTH: int = int(os.getenv('MAX_CONTENT_LENGTH', MAX_FILE_SIZE))
+
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
-    SECRET_KEY: str = os.getenv('SECRET_KEY')
-    MAX_CONTENT_LENGTH: int = int(os.getenv('MAX_CONTENT_LENGHT', MAX_FILE_SIZE))
-    CORS_SUPPORTS_CREDENTIALS: bool = True
+    JWT_SECRET_KEY: str = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES: int = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', timedelta(days=1)))
+    JWT_REFRESH_TOKEN_EXPIRES: int = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES', timedelta(days=30)))
+    JWT_COOKIE_DOMAIN: str = os.getenv('JWT_COOKIE_DOMAIN')
 
-    API_TITLE: str = os.getenv('API_TITLE')
-    API_VERSION: str = os.getenv('API_VERSION')
-    OPENAPI_VERSION: str = os.getenv('OPENAPI_VERSION')
+    CORS_SUPPORTS_CREDENTIALS = True
 
-    JWT_SECRET: str = os.getenv('JWT_SECRET')
-    JWT_ACCESS_TOKEN_EXPIRES: timedelta = timedelta(days=1)
+    API_TITLE = os.getenv('API_TITLE')
+    API_VERSION = os.getenv('API_VERSION')
+    OPENAPI_VERSION = os.getenv('OPENAPI_VERSION', '3.0.2')
 
 
 class ProductionConfig(_Config):
     DEBUG: bool = False
+    PROPAGATE_EXCEPTIONS: bool = False
+
     SQLALCHEMY_DATABASE_URI: str = os.getenv('PRODUCTION_DATABASE_URI')
-    CORS_ORIGINS: list[str] = [origin.strip() for origin in os.getenv('CORS_ORIGINS', '')]
-    if not CORS_ORIGINS:
-        raise ValueError('Variável CORS_ORIGINS não encontrada ou vazia')
+
+    CORS_RESOURCES: list[str] = os.getenv('CORS_RESOURCES', '/')
+    CORS_ORIGINS: list[str] = [origin.strip() for origin in os.getenv('CORS_ORIGINS', '').split(',')]
 
     OPENAPI_URL_PREFIX: str = None
     OPENAPI_SWAGGER_UI_PATH: str = None
     OPENAPI_SWAGGER_UI_URL: str = None
-    PROPAGATE_EXCEPTIONS: bool = False
 
 
 class DevelopmentConfig(_Config):
     DEBUG: bool = True
+    PROPAGATE_EXCEPTIONS: bool = True
+
     SQLALCHEMY_DATABASE_URI: str = os.getenv('DEVELOPMENT_DATABASE_URI')
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO: bool = True
+
     CORS_ORIGINS: list[str] = ['*']
 
-    OPENAPI_URL_PREFIX: str = os.getenv('OPENAPI_URL_PREFIX')
-    OPENAPI_SWAGGER_UI_PATH: str = os.getenv('OPENAPI_SWAGGER_UI_PATH')
-    OPENAPI_SWAGGER_UI_URL: str = os.getenv('OPENAPI_SWAGGER_UI_URL')
-    PROPAGATE_EXCEPTIONS: bool = True
+    OPENAPI_URL_PREFIX: str = os.getenv('OPENAPI_URL_PREFIX', '/')
+    OPENAPI_SWAGGER_UI_PATH: str = os.getenv('OPENAPI_SWAGGER_UI_PATH', '/swagger-ui')
+    OPENAPI_SWAGGER_UI_URL: str = os.getenv('OPENAPI_SWAGGER_UI_URL', 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/')
 
     API_SPEC_OPTIONS: dict[str, Any] = {
         'security': [{'bearerAuth': []}],
@@ -86,5 +93,8 @@ class DevelopmentConfig(_Config):
 
 class TestingConfig(_Config):
     TESTING: bool = True
+    PROPAGATE_EXCEPTIONS: bool = True
+
     SQLALCHEMY_DATABASE_URI: str = os.getenv('TEST_DATABASE_URI')
+
     CORS_ORIGINS: list[str] = ['*']
