0f56d5db59a9c84d894eaa52635eaa1f44fe5a53
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-07-28 09:19:44 -0300

    fix: corrigido erro na verificação de JWT para usuários inativos

--- app/interceptors.py
+++ app/interceptors.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask_jwt_extended import current_user, get_jwt_identity, verify_jwt_in_request
+from flask_jwt_extended import current_user, verify_jwt_in_request
 from werkzeug.exceptions import Forbidden
 
 
 def register_interceptors(app: Flask):
     @app.before_request
     def verify_user_is_active():
-        verify_jwt_in_request(optional=True)
-
-        if get_jwt_identity() and not current_user.ativo:
+        jwt = verify_jwt_in_request(optional=True)
+        if jwt is not None and not current_user.ativo:
             raise Forbidden('Este usuário está desativado')
