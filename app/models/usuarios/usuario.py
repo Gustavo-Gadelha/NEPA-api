c85e5b00066851fbefba9dbf53e7b2618bfdfa3a
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-08-26 00:57:13 -0300

    feat: implementa ModelManager e AccessManager no Model Usuario

--- app/models/usuarios/usuario.py
+++ app/models/usuarios/usuario.py
@@ -1,11 +1,28 @@
 import uuid
 
+from flask_jwt_extended import current_user
+
+from app.core import Manager, ModelManager, auto_managed
 from app.extensions import db
 from app.models.enums import Autoridade
 from app.models.mixins import TimestampMixin
 
 
+class AccessManager(Manager):
+    @staticmethod
+    def is_admin():
+        return current_user.autoridade == Autoridade.ADMIN
+
+    @staticmethod
+    def is_owner(owner_id):
+        return current_user.id == owner_id
+
+
+@auto_managed
 class Usuario(db.Model, TimestampMixin):
+    objects = ModelManager()
+    access = AccessManager()
+
     id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     nome = db.Column(db.String(255), nullable=False)
     matricula = db.Column(db.String(20), unique=True, nullable=False, index=True)
